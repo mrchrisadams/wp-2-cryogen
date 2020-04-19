5ab087100f0eec7cc0dbf77b8733161d95f60124
Chris Adams <chris@productscience.co.uk>
2020-04-19 21:49:31 +0200

    Reformat using cljformat
    Oh, so THAT's how it's supposed to look

--- src/wordpress_to_markdown/core.clj
+++ src/wordpress_to_markdown/core.clj
@@ -24,34 +24,37 @@
   "Returns a lazy seq of maps that correspond to each post item in the xml"
   [parsed-xml]
   (zip-xml/xml-> parsed-xml
-    :channel
-    :item [(keyword "wp:post_type") "post"]))
+                 :channel
+                 :item [(keyword "wp:post_type") "post"]))
 
-(defn is-markdown-status
+(defn post-is-markdown?
+  "Return boolean for whether post is written as markdown or not"
   [post]
-  ; TODO
-  ; :markdown (zip-xml/xml1-> post (keyword "wp:postmeta") (keyword "wp:meta_key")
-  ; <wp:meta_key>_wpcom_is_markdown</wp:meta_key>
-  )
+  (boolean (read-string
+            (zip-xml/xml1-> post
+                            (keyword "wp:postmeta")
+                            ; only return the value that marks if it's markdown
+                            [(keyword "wp:meta_key") "_wpcom_is_markdown"]
+                            ; then fetch the value out the xml
+                            (keyword "wp:meta_value")
+                            zip-xml/text))))
 
 (defn post->map
   "Return a map for the corresponding post we pass in."
   [post]
 
-  {
-    :author      (zip-xml/xml1-> post (keyword "dc:creator")  zip-xml/text)
-    :title       (zip-xml/xml1-> post :title zip-xml/text)
-    :content     (zip-xml/xml1-> post (keyword "content:encoded") zip-xml/text)
-    :status      (zip-xml/xml1-> post (keyword "wp:status") zip-xml/text)
-    :date        (zip-xml/xml1-> post (keyword "wp:post_date") zip-xml/text)
-    :post-slug   (zip-xml/xml1-> post (keyword "wp:post_name") zip-xml/text)
-    :is-markdown (post-is-markdown? post)
-    })
+  {:author      (zip-xml/xml1-> post (keyword "dc:creator")  zip-xml/text)
+   :title       (zip-xml/xml1-> post :title zip-xml/text)
+   :content     (zip-xml/xml1-> post (keyword "content:encoded") zip-xml/text)
+   :status      (zip-xml/xml1-> post (keyword "wp:status") zip-xml/text)
+   :date        (zip-xml/xml1-> post (keyword "wp:post_date") zip-xml/text)
+   :post-slug   (zip-xml/xml1-> post (keyword "wp:post_name") zip-xml/text)
+   :is-markdown (post-is-markdown? post)})
 
 (defn render-into-md-template
   [post-map]
   (let [ctx (dissoc post-map :content)]
-  (sp/render-file "./markdown-template.md" {:content (post-map :content) :ctx ctx})))
+    (sp/render-file "./markdown-template.md" {:content (post-map :content) :ctx ctx})))
 
 (defn parse-date
   [date-string]
@@ -66,25 +69,11 @@
   [post-map]
   (let [tmpl-string (render-into-md-template post-map)
         post-path (parse-path post-map)]
-        post-path))
+    post-path))
 
 (defn write-post-to-file
   [post-map output-directory]
   (spit (str output-directory (file-path-for-post post-map)) (render-into-md-template post-map)))
-
-(defn post-is-markdown?
-  "Return boolean for whether post is written as markdown or not"
-  [post]
-  (boolean (read-string
-    (zip-xml/xml1-> post
-      (keyword "wp:postmeta")
-      ; only return the value that marks if it's markdown
-      [(keyword "wp:meta_key") "_wpcom_is_markdown"]
-      ; then fetch the value out the xml
-      (keyword "wp:meta_value")
-      zip-xml/text
-
-    ))))
 
 (defn write-posts-to-markdown-files
   "Accepts a path to an wordpress xml file, output directory,
@@ -96,8 +85,7 @@
     (write-post-to-file post output-directory)))
 
 (let [[post & rest] (posts (load-xml-export blog-file))]
-  (post-is-markdown? post)
-)
+  (post-is-markdown? post))
 
 (defn fetch-first-post
   "Return the first post from the xml export."
@@ -105,4 +93,20 @@
   (let [[post & rest] (posts (load-xml-export blog-file))]
     (clojure.zip/node post)))
 
-(clojure.pprint/pprint (:content (fetch-first-post blog-file)))
+(fetch-first-post)
+
+; (clojure.pprint/pprint (:content (fetch-first-post blog-file)))
+
+; (def posts-as-nodes
+;   (-> blog-file
+;       load-xml-export
+;       posts
+;       count
+;       ))
+
+;     posts-as-nodes
+
+
+(let [loaded-posts (posts (load-xml-export blog-file))
+      [post & rest] loaded-posts]
+  (post->map post))
